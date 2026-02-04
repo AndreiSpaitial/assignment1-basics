8d11aedfa757c53ed278b0fd1cb3c704906e6ff1
Andrei-Alin Popescu <andrei@moonvalley.com>
2026-02-04 20:23:49 +0000

    Sped up slightly

--- cs336_basics/tokenizer/bpe_tokenizer.py
+++ cs336_basics/tokenizer/bpe_tokenizer.py
@@ -30,21 +30,30 @@ class BPETokenizer:
         input_str_utf8 = input_str.encode("utf-8")
         return tuple(bytes([el]) for el in input_str_utf8)
 
-    def _pretokenize_chunk(self, chunk: str) -> dict[str, int]:
+    def _token_pairs(self, pretoken: tuple[bytes]) -> Counter[tuple[bytes], int]:
+        return Counter((p1, p2) for p1,p2 in pairwise(pretoken))
+
+    def _pretokenize_chunk(self, chunk: str) -> tuple[dict[tuple[bytes], int], dict[tuple[bytes], Counter[tuple[bytes], int]]]:
         split_regex = "|".join(re.escape(el) for el in self.special_tokens)
         ret = Counter()
+        pairs_cache: dict[tuple[bytes], Counter[tuple[bytes], int]] = {}
 
         for doc in re.split(split_regex, chunk):
             for pre_token in re.finditer(PRETOKENIZER_PAT, doc):
-                ret[self._str_to_bytes_tuple(pre_token.group())] += 1
+                pretoken = self._str_to_bytes_tuple(pre_token.group())
+                if pretoken not in pairs_cache:
+                    pairs_cache[pretoken] = self._token_pairs(pretoken)
+                
+                ret[pretoken] += 1
         
-        return ret
+        return ret, pairs_cache
     
-    def _pretokenize(self, file_name) -> Counter[tuple[bytes], int]:
+    def _pretokenize(self, file_name) -> tuple[Counter[tuple[bytes], int], dict[tuple[bytes], Counter[tuple[bytes], int]]]:
         with open(file_name, "rb") as f:
             boundaries = find_chunk_boundaries(f, self.num_processes, b"<|endoftext|>")
 
-            total_freqs = Counter()
+            pretoken_freqs = Counter()
+            pairs_cache = {}
             # The following is a serial implementation, but you can parallelize this
             # by sending each start/end pair to a set of processes.
             for start, end in zip(boundaries[:-1], boundaries[1:]):
@@ -52,10 +61,15 @@ class BPETokenizer:
                 chunk = f.read(end - start).decode("utf-8", errors="ignore")
                 # Run pre-tokenization on your chunk and store the counts for each pre-token
 
-                counts = self._pretokenize_chunk(chunk)
-                total_freqs += counts
-            
-        return total_freqs
+                pretoken_freqs_chunk, pairs_cache_chunk = self._pretokenize_chunk(chunk)
+                pretoken_freqs += pretoken_freqs_chunk
+
+                for pretoken, pairs in pairs_cache_chunk.items():
+                    if pretoken in pairs_cache:
+                        continue
+                    pairs_cache[pretoken] = pairs
+
+        return pretoken_freqs, pairs_cache
 
     def _merge_pretoken(self, pretoken: tuple[bytes], pair: tuple[bytes]) -> tuple[bytes]:
         i=0
@@ -67,14 +81,17 @@ class BPETokenizer:
         
         return pretoken
 
-    def _merge(self, total_freqs: Counter[tuple[bytes], int]) -> Counter[tuple[bytes], int]:
-        pair_freqs = defaultdict(lambda:0)
-        for pretoken, freq in total_freqs.items():
-            for b1,b2 in pairwise(pretoken):
-                pair_freqs[tuple([b1,b2])] += freq
-        
+    def _merge(
+            self, 
+            pairs_freqs: Counter[tuple[bytes], int],
+            pairs_cache: dict[tuple[bytes], Counter[tuple[bytes], int]],
+            pretoken_freqs: Counter[tuple[bytes], int],
+        ) -> tuple[dict[tuple[bytes], Counter[tuple[bytes], int]], Counter[tuple[bytes], int]]:
+
         max_pair, max_freq = None,None
-        for pair,freq in pair_freqs.items():
+        for pair,freq in pairs_freqs.items():
+            if freq == 0:
+                continue
             if max_pair is None:
                 max_pair = pair
             if max_freq is None:
@@ -83,29 +100,58 @@ class BPETokenizer:
             max_freq, max_pair = max((max_freq, max_pair), (freq, pair))
         
         if max_pair is None: # no more merges possible, all pre-tokens are a single bytes object
-            return
+            return None, None
 
         self.merges.append(max_pair)
         self.dictionary.append(max_pair[0]+max_pair[1])
 
-        ret = Counter()
-        for pretoken, freq in total_freqs.items():
-            new_pretoken = self._merge_pretoken(pretoken, max_pair)
-            ret[new_pretoken] = freq
+        new_pairs_cache = {}
+        new_pretoken_freqs = {}
 
-        return ret
+        for pretoken, pairs in pairs_cache.items():
+            pretoken_freq = pretoken_freqs[pretoken]
+            if max_pair not in pairs:
+                new_pairs_cache[pretoken] = pairs
+                new_pretoken_freqs[pretoken] = pretoken_freq
+                continue
+
+            new_pretoken = self._merge_pretoken(pretoken, max_pair)
+            new_pairs = self._token_pairs(new_pretoken)
+            new_pairs_cache[new_pretoken] = new_pairs
+
+            for pair, freq in pairs.items():
+                new_freq = new_pairs[pair]
+                delta = new_freq - freq
+                pairs_freqs[pair] += pretoken_freq*delta
+            
+            for new_pair, freq in new_pairs.items():
+                if new_pair in pairs:
+                    continue
+                new_pair_freq = freq * pretoken_freq
+                pairs_freqs[new_pair] += new_pair_freq
+            
+            new_pretoken_freqs[new_pretoken] = pretoken_freq
+
+        return new_pairs_cache, new_pretoken_freqs
 
     def train(self, file_name: str) -> None:
         start_time = time.time()
-        total_freqs = self._pretokenize(file_name)
+        pretoken_freqs, pairs_cache = self._pretokenize(file_name)
         end_time = time.time()
 
         print(f"Pre-tokenization done in {(end_time-start_time)}s")
 
+
+        pairs_freqs: Counter[tuple[bytes], int] = Counter()
+        for pretoken, pairs in pairs_cache.items():
+            freq = pretoken_freqs[pretoken]
+            for pair, pretoken_pair_freq in pairs.items():
+                pairs_freqs[pair] += freq*pretoken_pair_freq
+
         i = 0
         with tqdm(total=self.vocab_size) as pbar:
-            while total_freqs is not None and len(self.dictionary) < self.vocab_size:
-                total_freqs = self._merge(total_freqs)
+            while pairs_cache is not None and len(self.dictionary) < self.vocab_size:
+                pairs_cache, pretoken_freqs = self._merge(pairs_freqs, pairs_cache, pretoken_freqs)
                 pbar.update(1)
 
     def tokenize(self, document: str) -> list[bytes]:
