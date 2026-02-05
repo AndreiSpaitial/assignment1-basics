958ccc972145384a5f848ba5234f06d0956218f6
Andrei-Alin Popescu <andrei@moonvalley.com>
2026-02-05 13:56:04 +0000

    .

--- cs336_basics/tokenizer/tests/test_bpe_tokenizer.py
+++ cs336_basics/tokenizer/tests/test_bpe_tokenizer.py
@@ -1032,6 +1032,37 @@ EXPECTED_MERGES = [
             (b' ',): 1,
         }
     },
+    {
+        "pairs_freqs": Counter(
+            {
+                (b' ', b'kabhbhkabh'): 1,
+                (b' ', b'xxbc'): 1,
+                (b' ', b'xbc'): 1,
+            }
+        ),
+        "pairs_cache": _reverse_pair_index({
+            (b' ', b'kabhbhkabh'):  Counter({
+                    (b' ', b'kabhbhkabh'): 1,
+            }),
+            (b' ', b'xbc'): Counter({
+                (b' ', b'xbc'): 1,
+            }),
+            (b' ', b'xxbc'): Counter({
+                (b' ', b'xxbc'): 1,
+            }),
+        }),
+        "pretoken_freqs": {
+            (b' bxz',): 3,
+            (b' kabhkabh',): 3,
+            (b'kabhkabh',): 1,
+            (b' ', b'kabhbhkabh'): 1,
+            (b' abc',): 3,
+            (b' abxy',): 3,
+            (b' ', b'xbc'): 1,
+            (b' ', b'xxbc'): 1,
+            (b' ',): 1,
+        }
+    },
 ]
 
 
