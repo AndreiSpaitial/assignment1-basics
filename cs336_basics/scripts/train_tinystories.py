d617f6f57c8aae1dc0cdf35df7f88ce3f4d9d8a6
Andrei-Alin Popescu <andrei@moonvalley.com>
2026-02-06 14:49:23 +0000

    Train tinystories

---
+++ cs336_basics/scripts/train_tinystories.py
@@ -0,0 +1,16 @@
+from cs336_basics.tokenizer import BPETokenizer
+
+
+if __name__ == "__main__":
+    bpe_tokenizer = BPETokenizer(
+        special_tokens=["<|endoftext|>"],
+        num_processes=64,
+        checkpoint_dir="cs336_basics/tokenizer/checkpoints/tinystories/",
+    )
+
+    bpe_tokenizer.train(
+        "data/TinyStoriesV2-GPT4-train.txt",
+        save_every=500,
+    )
+
+    bpe_tokenizer.save_checkpoint()
